4b4628cdb02da2a9a6e11b6ec4dd03c3df20014e
unknown <mar6269@OH1380-14.main.ad.rit.edu>
2015-03-20 11:13:15 -0400

    Battle Class stubs added

---
+++ RIT_RPG/RIT_RPG/Battle.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+
+namespace RIT_RPG
+{
+    //for simple Battles
+    class Battle
+    {
+        //attributes
+        protected PlayerChar c1;
+        protected PlayerChar c2;
+        protected PlayerChar c3;
+        protected PlayerChar c4;
+        private List<Enemy> enemies;
+
+        //properties
+
+
+        //Constructors
+
+        /// <summary>
+        /// Battle with 2 enemies
+        /// </summary>
+        /// <param name="char1">CoffeeGuy</param>
+        /// <param name="char2">WittyGuy</param>
+        /// <param name="char3">SmartGuy</param>
+        /// <param name="char4">FunnyGuy</param>
+        /// <param name="enem1"></param>
+        /// <param name="enem2"></param>
+        /// <param name="enem3"></param>
+        public Battle(CoffeeGuy char1, WittyGuy char2, SmartGuy char3, FunnyGuy char4, Enemy enem1, Enemy enem2)
+        {
+            c1 = char1;
+            c2 = char2;
+            c3 = char3;
+            c4 = char4;
+            enemies.Add(enem1);
+            enemies.Add(enem2);
+        }
+
+        /// <summary>
+        /// Battle with 3 enemies
+        /// </summary>
+        /// <param name="char1">CoffeeGuy</param>
+        /// <param name="char2">WittyGuy</param>
+        /// <param name="char3">SmartGuy</param>
+        /// <param name="char4">FunnyGuy</param>
+        /// <param name="enem1"></param>
+        /// <param name="enem2"></param>
+        /// <param name="enem3"></param>
+        public Battle(CoffeeGuy char1, WittyGuy char2, SmartGuy char3, FunnyGuy char4, Enemy enem1, Enemy enem2, Enemy enem3)
+        {
+            c1 = char1;
+            c2 = char2;
+            c3 = char3;
+            c4 = char4;
+            enemies.Add(enem1);
+            enemies.Add(enem2);
+            enemies.Add(enem3);
+        }
+
+        /// <summary>
+        /// Battle with 4 enemies
+        /// </summary>
+        /// <param name="char1">CoffeeGuy</param>
+        /// <param name="char2">WittyGuy</param>
+        /// <param name="char3">SmartGuy</param>
+        /// <param name="char4">FunnyGuy</param>
+        /// <param name="enem1"></param>
+        /// <param name="enem2"></param>
+        /// <param name="enem3"></param>
+        public Battle(CoffeeGuy char1, WittyGuy char2, SmartGuy char3, FunnyGuy char4, Enemy enem1, Enemy enem2, Enemy enem3, Enemy enem4)
+        {
+            c1 = char1;
+            c2 = char2;
+            c3 = char3;
+            c4 = char4;
+            enemies.Add(enem1);
+            enemies.Add(enem2);
+            enemies.Add(enem3);
+            enemies.Add(enem4);
+        }
+
+        //Methods
+        //commented out
+        //public int TakeDamage()
+        //{
+            //subtract / add health,
+            
+            //check if players or enemies are dead
+            
+            //check if Battle is over or Game Over
+            
+        //}
+        //commented out
+
+        /// <summary>
+        ///  this method will handle all of the meat of the battle
+        /// </summary>
+        public void RunBattle()
+        {
+            // loop to see if either enemy party or player party is dead
+
+            // inside loop determine who goes first based on speed
+
+            // if player characters go first then call methods depending on selection inside the battle menu
+
+            // if enemys go first then they randomly target a player character and call appropriate method
+
+            // if bettle ends then call endBattle or gameOver method
+        }
+
+        /// <summary>
+        /// Brings up the battle Menu and the battlescreen
+        /// </summary>
+        public void DrawScreen()
+        {
+            //Draw Background 
+
+            //Draw menu
+            
+            //Draw Enemies and player characters
+        }
+
+        /// <summary>
+        /// Ends the battle if the player wins
+        /// </summary>
+        public void EndBattle()
+        {
+            //allocate EXP
+            //level up?
+            //Draw Overworld
+        }
+
+        /// <summary>
+        /// if all allies are dead, Draw Game Over Screen
+        /// </summary>
+        public void GameOver()
+        {
+            //draw Game Over screen
+        }
+
+
+
+        
+
+    }
+}
---
+++ RIT_RPG/RIT_RPG/BossBattle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+
+namespace RIT_RPG
+{
+    //Boss Battles, EX: Dresler or other bosses we want
+    class BossBattle : Battle
+    {
+        //attributes - unique to Boss Fight
+        private BossEnemy boss;
+
+        //properties
+
+        //Constructor
+
+        /// <summary>
+        /// Creates the Boss battle, usual 4 player characters and 1 boss
+        /// </summary>
+        /// <param name="char1">CoffeeGuy</param>
+        /// <param name="char2">WittyGuy</param>
+        /// <param name="char3">SmartGuy</param>
+        /// <param name="char4">FunnyGuy</param>
+        /// <param name="bossChar"> accepts the bosses into the parameter</param>
+        public BossBattle(CoffeeGuy char1, WittyGuy char2, SmartGuy char3, FunnyGuy char4, BossEnemy bossChar)
+        {
+            //set the variables to their respective variables (final names subject to change)
+            c1 = char1;
+            c2 = char2;
+            c3 = char3;
+            c4 = char4;
+            boss = bossChar;
+        }
+
+
+    }
+}
